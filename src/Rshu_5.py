14237263a02397867e14438d507a8aee00856004
MuggleK <775847312@qq.com>
2021-08-12 08:47:34 +0800

    Rshu-5

--- src/Rshu_5.py
+++ src/Rshu_5.py
@@ -5,6 +5,18 @@ import time
 from loguru import logger
 
 
+def get_ip():
+    while True:
+        try:
+            ip = requests.get(url='http://219.151.149.149:8888/get_ips/?user_code=688688&user_key=688688').text.split('\n')[0]
+            return {
+                'https': ip,
+                'http': ip
+            }
+        except:
+            pass
+
+
 class Rshu5:
 
     def __init__(self, url, ts_url, cookie_s, cookie_t):
@@ -15,6 +27,7 @@ class Rshu5:
             'Cache-Control': 'no-cache',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
         }
+        self.proxy = get_ip()
         with open('./Rshu.js','r',encoding='utf-8')as f:
             self.ev = f.read()
         self.url = url
@@ -26,14 +39,14 @@ class Rshu5:
         self.process_content()
 
     def get_content(self):
-        res = self.session.get(self.url, verify=False)
+        res = self.session.get(self.url, verify=False, proxies=self.proxy)
         self.cookie_80s = res.cookies.get_dict().get(self.cookie_name_1)
         content = re.findall('<meta content="(.*?)">', res.text)[0]
         js_code = re.findall(r'(\(function\(\).*\(\))</script>', res.text)[0]
         return content, js_code
 
     def get_ts(self):
-        res = self.session.get(self.ts_url)
+        res = self.session.get(self.ts_url, proxies=self.proxy)
         ts_code = "window = global;document={};" + res.text
         try:
             temp_flag = re.findall(r'(.{4}=_\$.{2}\[_\$.{2}\[\d{2}\]\]\(_\$.{2},(.*?)\))', self.js_code)[0]
@@ -69,7 +82,7 @@ class Rshu5:
         self.session.headers.update({
             'cookie': f'{self.cookie_name_1}={self.cookie_80s};{self.cookie_name_2}={self.cookie_80t}'
         })
-        res = self.session.get(url = self.url,headers = self.session.headers)
+        res = self.session.get(url = self.url,headers = self.session.headers, proxies=self.proxy)
         res.encoding = res.apparent_encoding
         if res.status_code == 200:
             return self.session.headers.get('cookie')
@@ -78,14 +91,15 @@ class Rshu5:
 
 
 if __name__ == '__main__':
-    startTime = time.time()
-    cookie_s = 'goN9uW4i0iKzS'
-    cookie_t = 'goN9uW4i0iKzT'
-    base_url = 'http://wcjs.sbj.cnipa.gov.cn/txnT01.do'
-    ts_url = 'http://wcjs.sbj.cnipa.gov.cn/c5rxzYrjRT2h/cCdzB9ZjDFks.9887446.js'
-    temp_gx = Rshu5(base_url, ts_url, cookie_s, cookie_t)
-    cookies = temp_gx.verify()
-    if cookies:
-        logger.success(f'base_url -> {base_url} -> {cookies}')
-        costTime = format(time.time() - startTime, '.2f')
-        logger.debug(f'Total Cost: {costTime}s')
+    cookie_s = 'wIlwQR28aVgbS'
+    cookie_t = 'wIlwQR28aVgbT'
+    base_url = 'http://epub.cnipa.gov.cn/flzt.jsp'
+    ts_url = 'http://epub.cnipa.gov.cn/DMkKrRq2vkXD/3lyVKWvJXsxS.d5db026.js'
+    while True:
+        startTime = time.time()
+        temp_gx = Rshu5(base_url, ts_url, cookie_s, cookie_t)
+        cookies = temp_gx.verify()
+        if cookies:
+            logger.success(f'base_url -> {base_url} -> {cookies}')
+            costTime = format(time.time() - startTime, '.2f')
+            logger.debug(f'Total Cost: {costTime}s')
